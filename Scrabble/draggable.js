7fa1052700b4e3ee9979f7a7b333034ccf35678c
Loup Meurice <loup.meurice@unamur.be>
2022-12-19 15:42:22 +0100

    point panel added

--- Scrabble/draggable.js
+++ Scrabble/draggable.js
@@ -317,3 +317,5 @@ function dragMiniElement(elmnt) {
 
   
 }
+
+
---
+++ Scrabble/score.js
@@ -0,0 +1,16 @@
+positionScore();
+function positionScore() {
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    let score = document.getElementById("score");
+    if(w > h) {
+        //document.getElementById("score").style.top = 0;
+        score.classList.add("leftScore");
+        score.classList.remove("bottomScore");
+    } else {
+        score.classList.add("bottomScore");
+        score.classList.remove("leftScore");
+    }
+}
+
+onresize = (event) => {positionScore()};
